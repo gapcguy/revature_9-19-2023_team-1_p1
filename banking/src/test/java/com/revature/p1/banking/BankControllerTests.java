85d6f386780c8a9e1db82fa664bf7dcea8ca3fe8
tcurtis526 <tac526@gmail.com>
2023-09-20 16:13:03 -0400

    Made empty tests to be created.

--- banking/src/test/java/com/revature/p1/banking/BankControllerTests.java
+++ banking/src/test/java/com/revature/p1/banking/BankControllerTests.java
@@ -1,4 +1,79 @@
 package com.revature.p1.banking;
 
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class BankControllerTests {
+    @BeforeAll
+    void setup(){
+
+    }
+
+    @AfterAll
+    void close(){
+
+    }
+
+
+    //User controller tests
+    @Test
+    void testLogin(){
+
+    }
+
+    @Test
+    void testRegistration(){
+
+    }
+
+    @Test
+    void testDeleteUser(){
+
+    }
+
+    //Account controller tests
+    @Test
+    void testCreateAccount(){
+
+    }
+
+    @Test
+    void testDeleteAccount(){
+
+    }
+
+    @Test
+    void testGetAccounts(){
+
+    }
+
+    @Test
+    void testGetSpecificAccount(){
+
+    }
+
+    //Transactions
+    @Test
+    void testCreateTransaction(){
+
+    }
+
+    @Test
+    void testGetTransactions(){
+
+    }
+
+    @Test
+    void testGetTransactionsForAccount(){
+
+    }
+
+    @Test
+    void testGetTransactionsForUser(){
+
+    }
+
+    //Testing Miscellaneous
 }

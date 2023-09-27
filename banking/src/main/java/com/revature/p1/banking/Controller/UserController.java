ee66a51228ba74990b9d479bd258930e52d93e62
akashkumar1691 <akashkumar1691@berkeley.edu>
2023-09-27 12:29:43 -0700

    initialiized userService in userController

--- banking/src/main/java/com/revature/p1/banking/Controller/UserController.java
+++ banking/src/main/java/com/revature/p1/banking/Controller/UserController.java
@@ -1,8 +1,10 @@
 package com.revature.p1.banking.Controller;
 
 import com.revature.p1.banking.Models.User;
+import com.revature.p1.banking.Service.TransactionService;
 import com.revature.p1.banking.Service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,16 +16,19 @@ import java.util.List;
 @CrossOrigin
 public class UserController {
 
-    private final UserService userService;
+    private final UserService uServ;
+
+    @Autowired
+    public UserController(UserService uServ) { this.uServ = uServ; }
 
     @PreAuthorize(value = "hasRole('manager')")
     @GetMapping("")
     public List<User> getUsers() {
-        return userService.findAll();
+        return uServ.findAll();
     }
 
     @PostMapping("/register")
     public User addUser(@RequestBody User u) {
-        return userService.insertUser(u);
+        return uServ.insertUser(u);
     }
 }
